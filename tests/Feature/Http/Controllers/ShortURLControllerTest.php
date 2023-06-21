8240a7a5c54872992926d1efc60f199cfb3f18a3
Mousemices <9.12716135xia@gmail.com>
2023-06-21 11:46:40 +0200

    [ADD] Test for ShortURLController.

--- tests/Feature/Http/Controllers/ShortURLControllerTest.php
+++ tests/Feature/Http/Controllers/ShortURLControllerTest.php
@@ -8,15 +8,17 @@ use Tests\TestCase;
 
 class ShortURLControllerTest extends TestCase
 {
+    use RefreshDatabase;
+
     protected function setUp(): void
     {
         parent::setUp();
         $this->actingAs(User::factory()->create());
     }
 
-    public function test_returns_list_of_short_url(): void
+    public function test_displays_create_short_url_page(): void
     {
-        $response = $this->get(route('short-url.index'));
+        $response = $this->get(route('short-url.create'));
 
         $response->assertStatus(200)
             ->assertSessionHasNoErrors();
@@ -26,6 +28,23 @@ class ShortURLControllerTest extends TestCase
     {
         $this->post(route('short-url.generate'), [
             'destination_url' => 'www.baidu.com',
+            'short_code' => null
+        ]);
+
+        $this->assertDatabaseCount('short_urls', 1);
+    }
+
+    public function test_shortURL_can_be_created_with_custom_short_code(): void
+    {
+        $shortCode = 'baidu';
+
+        $this->post(route('short-url.generate'), [
+            'destination_url' => 'www.baidu.com',
+            'short_code' => $shortCode
+        ]);
+
+        $this->assertDatabaseHas('short_urls', [
+            'short_code' => $shortCode
         ]);
     }
 }
